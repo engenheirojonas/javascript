da04aa0c0a005b1c5ca234a4be3d3edf7766da5e
engenheirojonas <giona247.mg@gmail.com>
2026-05-25 15:48:24 +0100

    atualizar

---
+++ aulas com gustavo guanabara/modulo D/js_013b.js
@@ -0,0 +1,10 @@
+//para ver o comportamento deste programa tenta mudar o valor da variavel pais
+var pais = 'França'
+console.log(`O seu pais é ${pais}`)
+
+        //Condição Composta
+if (pais == 'Angola'){
+    console.log('Você, és Angolano')
+} else {
+    console.log('Você, és um Estrangeiro')
+}
