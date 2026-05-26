5921382357402b39e0d25fc4150ae6956969ef9a
engenheirojonas <giona247.mg@gmail.com>
2026-05-26 15:49:51 +0100

    atualizar

--- aulas com gustavo guanabara/modulo D/js_018.js
+++ aulas com gustavo guanabara/modulo D/js_018.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('img')
     var data = new Date()
-    var hora = 18//data.getHours()  //podes definir um valor fixo (exemplo: 13, 14, 20, ...) ao em vex de "data.getHours()"
+    var hora = data.getHours()  //podes definir um valor fixo (exemplo: 13, 14, 20, ...) ao em vex de "data.getHours()"
     var saudacao = window.document.getElementById('saudar')
     msg.innerHTML = `agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
